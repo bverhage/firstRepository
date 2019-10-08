8366d61fc1ae63949f7cba019f4b76984df443ab
bverhage <55459487+bverhage@users.noreply.github.com>
2019-10-08 15:57:01 +0200

    Add files via upload
    I addes something but yea it is currently a cluster fuck

--- 3D_rotationalandtranlational_numerical_tryout.py
+++ 3D_rotationalandtranlational_numerical_tryout.py
@@ -10,34 +10,66 @@ Created on Fri Sep 20 16:58:36 2019
 This is a numeric differential equation solver
 """
 print('------ begin of code ------')
+print('Current assumprions:Flat earh')
 ## preperation
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 ## world variables
 graf=float(9.81)
-## list of the active forces
+
 
 ##functions
 ## functions for numeric diff.eq solving
 def EF(Time,w,dt):
-    ### Euler forward numerical integration method
-    ### w_(n+1)=w_n+dt*f(tn,wn)
+    ''' Euler forward numerical integration method
+        w_(n+1)=w_n+dt*f(tn,wn)
+        
+        Computational easiest integration method and Analysticly worst
+        
+        input is a w matrix consisting of w=[w0,w1,w2,...,wn]
+        with wi=[u1(i),u2(i),u3(i),...,um(i)]^T
+        
+        Returns: wn+1=[u1(n+1),u2(n+1),...,un(n+1)]^T'''
+    
     ans=w[:,-1:]+dt*f(Time[-1],w[:,-1:]);
     return(ans)
 
 def TZ(Time,w,dt):
-    ### trapezodial numerical integration method
-    ### w_(n+1)=w_n+dt*(f(t_n,w_n)+f(tn+dt,w*_(n+1)))/2
-    ### with w*_(n+1)=w_n+dt*f(tn,wn)
+    ''' Trapezodial numerical integration method
+        w_(n+1)=w_n+dt*(f(t_n,w_n)+f(tn+dt,w*_(n+1)))/2
+        with w*_(n+1)=w_n+dt*f(tn,wn)
+        
+        Averge on computuational time and averge on analytics
+        
+        input is a w matrix consisting of w=[w0,w1,w2,...,wn]
+        with wi=[u1(i),u2(i),u3(i),...,um(i)]^T
+        
+        Returns: wn+1=[u1(n+1),u2(n+1),...,un(n+1)]^T'''
+        
+        
     
     ans= w[:,-1:]+dt/2*(f(Time[-1],w[:,-1:])+f(Time[-1]+dt,w[:,-1:]+dt*f(Time[-1],w[:,-1:])));
     
     return(ans)
     
 def RK(Time,w,dt):
-    ### Runge-Kutta integrtion method
+    ''' Runge-Kutta integrtion method
+        w_(n+1)=w_n+1/6(k1+2k2+2k3+k4)
+        
+        with    k1=dt*f(tn,wn)
+                k2=dt*f(tn+dt/2,w_n+k1/2) 
+                k3=dt*f(tn+dt/2,w_n+k2/2)
+                k4=dt*f(t_n+dt,w_n+k3)
+                
+        Computationaly the hardest method but analystically the best.
+        
+        input is a w matrix consisting of w=[w0,w1,w2,...,wn]
+        with wi=[u1(i),u2(i),u3(i),...,um(i)]^T
+        
+        Returns: wn+1=[u1(n+1),u2(n+1),...,un(n+1)]^T'''
     
     k1=dt*f(Time[-1],w[:,-1:])
     
@@ -56,23 +88,18 @@ def RK(Time,w,dt):
     
     ## the differential eq      
 def f(t,w):
+    ''' The differential equation that has to be solved.
+        imputs: t is time array
+                w matrix consisting of w=[w0,w1,w2,...,wn]
+                with wi=[u1(i),u2(i),u3(i),...,um(i)]^T
+                
+        Returns:The next numerical approximation of the solution.
+                wn+1=[u1(n+1),u2(n+1),...,un(n+1)]^T'''
     
-    #creating a place matrix
-    x=w[0:3]
-    #creating a speed matrix
-    x_dot=w[3:6]
 
-    #creating a place matrix
-    o=w[6:9]
-
-    #creating a speed matrix
-    o_dot=w[9:12]
-
-
-    
-    #interial matrix 
-    I=np.array([[3,0,0],
-                [0,1,0],
+    #Linear part of the differntial equation matrix 
+    I=np.array([[5,0,0],
+                [0,5,0],
                 [0,0,1]])
     
     
@@ -85,18 +112,42 @@ def f(t,w):
                 [np.zeros((6,6)),A]
                 ])
     
+    # this is purly for comsetical reasons.
+    #extracting a place matrix
+    x=w[0:3]
     
-    #the non homogenus part
-    ans=A.dot(w)+Internal(o_dot,I)+External(I)+Fg()+Fwl(x_dot)
+    #exctracting a speed matrix
+    x_dot=w[3:6]
+
+    #exctrating a place matrix
+    o=w[6:9]
+
+    #exctracting a speed matrix
+    o_dot=w[9:12]
+    
+    
+    # this is experimental code
+    cp=1
+    #rcpcp is the vector form the centere of mass to the centre of pressure.
+    #https://www.grc.nasa.gov/www/k-12/rocket/rktcp.html
+    rcmcp=np.transpose(
+            np.array(
+                    [[np.cos(np.pi/2-o[2,-1])*np.sin(o[1,-1])*cp],
+                    [np.cos(np.pi/2-o[2,-1])*np.cos(o[1,-1])*cp],
+                    [np.cos(np.pi/2-o[2,-1])*cp]]))
+    
 
     
-    return(ans)      
+    #the non linear part of the differential equation.
+    ans=A.dot(w)+Rot_Internal(o_dot,I)+Rot_External(I,x_dot,rcmcp)+Lat_Fg()+Lat_Fwl(x_dot)
+
+    return(ans)    
 
 
 
 
 ## Forces
-def Internal(o_dot,I):
+def Rot_Internal(o_dot,I):
 
     
     #linear part of the rotional acceleration
@@ -108,11 +159,10 @@ def Internal(o_dot,I):
                 ])
     return(ans)
     
-def External(I):
+def Rot_External(I,x_dot,rcmcp):
 
-    M=np.array([[0],
-                [0],
-                [0]])
+
+    M=np.transpose(np.cross(np.transpose(Lat_Fwl(x_dot)[3:6]),rcmcp))
     
     ans=np.block([
                 [np.zeros((9,1))],
@@ -120,13 +170,13 @@ def External(I):
                 ])
     return(ans)
 
-def Fg():
+def Lat_Fg():
     #Grafity
     ans=np.zeros([12,1])
     ans[5]=-graf
     return(ans)
     
-def Fwl(v):
+def Lat_Fwl(v):
     #air resitance
     Cw=float(0.75)
     Area=float(0.04)
@@ -139,59 +189,101 @@ def Fwl(v):
                 ])
     return(ans)
 
-## variables
+## Inital condition
+# first time step
 t0=0
 Time=[t0]
+#last time step
 tE=20
+#step time
 dt=0.1
-w0=np.array([[0],
+
+# inital conditions
+# This has no coding purpose. 
+# This is purly for clarivication
+
+#initial place vector
+x0=np.array([[0],
              [0],
-             [100],
+             [80]])
+    
+#intial speed vector
+x0_dot=np.array([[0],
+                 [0],
+                 [40]])
+
+#inital orientation vector
+o0=np.array([[0],
              [0],
-             [0],
-             [10],
-             [-1],
-             [0],
-             [1],
-             [0.5],
-             [1],
-             [-1]])
+             [1/2*np.pi]])  
+
+#inital angular velocity vector
+o0_dot=np.array([[0],
+                 [0],
+                 [0]]) 
+    
+
+#creating the initial w matrix
+# with the earlier defined inital conditions
+w0=np.block([
+                [x0],
+                [x0_dot],
+                [o0],
+                [o0_dot]
+                ])
 w=w0
 
-## The excecution
-while(Time[-1]<tE):
-    #solution
-    wn=RK(Time,w,dt)
-    w=np.append(w,wn,axis=1)
 
-    #time step
+
+##----------- The excecution --------------
+while(Time[-1]<tE):
+    
+    #appling the numerical method over the differnential eqation f.
+    
+    wn=RK(Time,w,dt)
+    
+    #adding the next point to the numerical matrix w
+    
+    w=np.append(w,wn,axis=1)
+    
+
+    #going to the next time step.
+    
     Time.append(Time[-1]+dt)
     
+    #reitterating everything until tE
+    
+    #unless the z coordinate has become 0.
+    if(w[2,-1]<0):
+        break
+    
 
     
     
-## plots
-    #creating a place matrix
-    x=w[0:3]
-    #creating a speed matrix
-    x_dot=w[3:6]
+##------------- The plots -----------------
+    
+#Creating these matrices is purley cosmetical.
+    
+#creating a place matrix
+x=w[0:3]
+#creating a speed matrix
+x_dot=w[3:6]
 
-    #creating a place matrix
-    o=w[6:9]
+#creating a place matrix
+o=w[6:9]
 
-    #creating a speed matrix
-    o_dot=w[9:12]
-
-#for i in range(len(Time)):
-#    Forces.append(FSUM(w[:,i:]))
+#creating a speed matrix
+o_dot=w[9:12]
 
 
-#fig1 = plt.figure()
-#ax = fig1.add_subplot(111, projection='3d')
-#ax.scatter(o[0,:], o[1,:], o[2,:], c='r', marker='o')
-#fig2 = plt.figure()
-#ax = fig2.add_subplot(111, projection='3d')
-#ax.scatter(Time, o[1,:], o[2,:], c='r', marker='o')
+#experimental code
+cp=1
+
+Rcmcp=np.array([[np.cos(np.pi/2-o[2])*np.sin(o[1])*cp],
+                [np.cos(np.pi/2-o[2])*np.cos(o[1])*cp],
+                [np.cos(np.pi/2-o[2])*cp]])
+
+
 fig1 = plt.figure()
 plt.subplot(211)
 plt.plot(Time,o_dot[0,:],'-',color='black')
@@ -209,12 +301,13 @@ plt.plot(Time,o[1,:],'-',color='blue')
 plt.plot(Time,o[2,:],'-',color='red')
 
 plt.title('orientation')
-plt.legend(('theta','phi','xi'))
+plt.legend(('p','q','r'))
 plt.show()
 
 fig2 = plt.figure()
 ax = fig2.add_subplot(111, projection='3d')
 ax.scatter(x[0,:], x[1,:], x[2,:], c='r', marker='o')
+ax.scatter(x[0,:]+Rcmcp[0,:], x[1,:]+Rcmcp[1,:], x[2,:]+Rcmcp[2,:], c='b', marker='*')
 
 fig3 = plt.figure()
 ax = fig3.add_subplot(121)
@@ -227,9 +320,8 @@ plt.plot(Time,x_dot[2,:],'-')
 plt.title('z_dot')
 plt.ylabel('z_dot(m/s)')
 plt.xlabel('Time(s)')
-#plt.subplot(313)
-#plt.plot(Time[0:-1],Forces,'-')
-#plt.show
+
+
 print('------ end of code ------')    
 
 
